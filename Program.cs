d4e31bbea8fa90520de028a80973d2eb12c3825d
Abdallah Hany <prog.eng.abdallah.hany@gmail.com>
2025-08-08 04:57:39 +0300

    day4
    day4

--- Program.cs
+++ Program.cs
@@ -1,136 +1,252 @@
 ﻿using System;
-using System.Globalization;
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Text;
+using System.Collections.Generic;
 
-namespace day3.sol
+namespace day4.sol
 {
     internal class Program
     {
-
-        class Animal
+        enum DayOfWeek
         {
-            public string Name;
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
         }
         static void Main(string[] args)
         {
+            //int[] array1 = new int[3] ;
+            //int [] array2 = new int[3] {1,2,3};
+            //int[] array3 = new int[] { 4, 5, 6 };
+            //int[] array4 =  {6,7, 8};
 
-
-
-            //(CTRL + / )--> comment / uncomment
-
-            string v = Console.ReadLine();
-            try
-            {
-                int x = int.Parse(v);
-                int y = Convert.ToInt32(v);
-                Console.WriteLine(v);
-                Console.WriteLine(x);
-                Console.WriteLine(y);
-            }
-            catch
-            {
-                Console.WriteLine("Invalid input");
-            }
-
-
-
-            string v = Console.ReadLine();
-            bool x = int.TryParse(v, out int y);
-            if (y == 0)
-            {
-                Console.WriteLine("Error");
-            }
-            else
-            {
-                Console.WriteLine(y);
-            }
+            //array1 [0] = 1;
+            //array1 [1] = 2;
+            //array1 [2] = 3;
+            //Console.WriteLine(array1[0]);
+            //Console.WriteLine(array1[1]);
+            //Console.WriteLine(array1[2]);//--> default is 0
+            //Console.WriteLine(array1[3]);//-->OutOfRangeException 
 
 
 
 
+            //int[] a1 = { 1, 2, 3 };
+            //int[] a2 = { 4, 5, 6 };
 
-            object obj1 = new object();
-            object obj2 = new object();
-            Console.WriteLine(obj1.GetHashCode());
-            Console.WriteLine(obj2.GetHashCode());
-            obj1 = "Ali";
-            Console.WriteLine(obj1);
-            obj1 = 2;
-            Console.WriteLine(obj1);
-            obj1 = 2.5;
-            Console.WriteLine(obj1);
-            obj2 = obj1;
-            Console.WriteLine(obj2);
-            Console.WriteLine(obj1.GetHashCode());
-            Console.WriteLine(obj2.GetHashCode());
+            //a1 = a2;
+            //Console.WriteLine(a2[1]);//5
+            //a1[1] = 8;
+            //Console.WriteLine(a2[1]);//8
+
+
+            //a1 = (int[])a2.Clone();
+            //Console.WriteLine(a2[1]);//5
+            //a1[1] = 8;
+            //Console.WriteLine(a2[1]);//5
 
 
 
 
+            //String[,] students = new string[3, 3];
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("Enter student name,subject,grade in sequence: ");
+            //    for (int j = 0; j < 3; j++)
+            //    {
 
-            Animal a1 = new Animal();
-            Animal a2 = new Animal();
-            a1.Name = "Cat";
-            a2 = a1;
-            Console.WriteLine(a1.Name);
-            Console.WriteLine(a2.Name);
-
-
-
-
-
-
-            String x = "I'm Abdallah";
-            Console.WriteLine(x.GetHashCode());
-            Console.WriteLine(x + " Hi Willy");
-            Console.WriteLine(x.GetHashCode());
-            Console.WriteLine($"{x} Hi willy");
-            Console.WriteLine(x.GetHashCode());
-            x += " Hi Willy";
-            Console.WriteLine(x);
-            Console.WriteLine(x.GetHashCode());
+            //        students[i, j] = Console.ReadLine();
+            //    }
+            //}
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        Console.Write(students[i, j]+" ");
+            //    }
+            //    Console.WriteLine();
+            //}
 
 
 
 
+            //int[] a = { 2, 6, 1, 4, 5};
+            //int[] b =new int[3] ;
 
-            StringBuilder x = new StringBuilder("Hey");
-            Console.WriteLine(x);
-            Console.WriteLine(x.GetHashCode());
-            x.Append(" Hi Willy");
-            Console.WriteLine(x);
-            Console.WriteLine(x.GetHashCode());
+            //Array.Sort(a);
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.Write(a[i] + " ");//1 2 4 5 6
+            //}
 
+            //Array.Reverse(a);
+            //for (int f = 0; f < a.Length; f++)
+            //{
+            //    Console.Write(a[f] + " ");//5 4 1 6 2
+            //}
+            //Console.WriteLine(Array.IndexOf(a,a));
 
+            //Array.Copy(a, b, 3);
+            //for (int d = 0; d < 3; d++)
+            //{
+            //    Console.WriteLine(b[d] + " ");//2 6 1
+            //}
 
-
-
-
-
-            int x = int.Parse(Console.ReadLine());
-            int y = int.Parse(Console.ReadLine());
-            Console.WriteLine("Sum is " + x + "+" + y + "=" + (x + y));
-            Console.WriteLine(string.Format("Sum is {0}+{1}={2}", x, y, (x + y)));
-            Console.WriteLine($"Sum is {x}+{y}={x + y}");
-
-
-
-
-
-
-            StringBuilder sb = new StringBuilder("Hello");
-            Console.WriteLine(sb.Append("World"));
-            Console.WriteLine(sb.Replace("ld", "y"));
-            Console.WriteLine(sb.Insert(0, "go "));
-            Console.WriteLine(sb.Remove(0, 2));
+            //Array.Clear(a, 3, 1);
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.Write(a[i] + " ");//2 6 1 0 5
+            //}
 
 
 
 
+            //int[] numbers = { 10, 20, 30, 40, 50 };
+
+            //Console.WriteLine("for loop:");
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            //Console.WriteLine("foreach loop:");
+            //foreach (int num in numbers)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            //Console.WriteLine("while loop in reverse:");
+            //int index = numbers.Length -1;
+            //while (index >= 0)
+            //{
+            //    Console.WriteLine(numbers[index]);
+            //    index--;
+            //}
 
 
-        }
-    }
-}
+
+
+            //int n;
+
+            //do
+            //{
+            //    Console.Write("Enter a positive odd number: ");
+            //    string i = Console.ReadLine();
+
+            //    if (int.TryParse(i, out n) && n > 0 && n % 2 != 0)
+            //    {
+            //        Console.WriteLine($"Correct! You entered {n}.");
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Invalid input");
+            //    }
+
+            //} while (true);
+
+
+
+
+            //    int[,] a =
+            //{
+            //    { 1,  2,  3 },
+            //    { 4,  5,  6 },
+            //    { 7,  8,  9 }
+            //};
+
+            //    Console.WriteLine("Matrix:");
+            //    for (int i = 0; i < a.GetLength(0); i++)
+            //    {
+            //        for (int j = 0; j < a.GetLength(1); j++)
+            //        {
+            //            Console.Write(a[i, j] + "  "); 
+            //        }
+            //        Console.WriteLine(); 
+            //    }
+
+
+
+
+            //Console.Write("Enter month number (1-12): ");
+            //int month = int.Parse(Console.ReadLine());
+
+            //if (month == 1) Console.WriteLine("January");
+            //else if (month == 2) Console.WriteLine("February");
+            //else if (month == 3) Console.WriteLine("March");
+            //else if (month == 4) Console.WriteLine("April");
+            //else if (month == 5) Console.WriteLine("May");
+            //else if (month == 6) Console.WriteLine("June");
+            //else if (month == 7) Console.WriteLine("July");
+            //else if (month == 8) Console.WriteLine("August");
+            //else if (month == 9) Console.WriteLine("September");
+            //else if (month == 10) Console.WriteLine("October");
+            //else if (month == 11) Console.WriteLine("November");
+            //else if (month == 12) Console.WriteLine("December");
+            //else Console.WriteLine("Invalid month number");
+
+            //switch (month)
+            //{
+            //    case 1: Console.WriteLine("January"); break;
+            //    case 2: Console.WriteLine("February"); break;
+            //    case 3: Console.WriteLine("March"); break;
+            //    case 4: Console.WriteLine("April"); break;
+            //    case 5: Console.WriteLine("May"); break;
+            //    case 6: Console.WriteLine("June"); break;
+            //    case 7: Console.WriteLine("July"); break;
+            //    case 8: Console.WriteLine("August"); break;
+            //    case 9: Console.WriteLine("September"); break;
+            //    case 10: Console.WriteLine("October"); break;
+            //    case 11: Console.WriteLine("November"); break;
+            //    case 12: Console.WriteLine("December"); break;
+            //    default: Console.WriteLine("Invalid month number"); break;
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+            //    int[] n = { 5, 10, 15, 20, 25 };
+
+            //    int sumFor = 0;
+            //    for (int i = 0; i < n.Length; i++)
+            //    {
+            //        sumFor += n[i];
+            //    }
+            //    Console.WriteLine($"Sum for loop: {sumFor}");
+
+            //    int sumForeach = 0;
+            //    foreach (int num in n)
+            //    {
+            //        sumForeach += num;
+            //    }
+            //    Console.WriteLine($"Sum foreach loop: {sumForeach}");
+
+
+
+
+
+
+
+
+            Console.Write("Enter a number (1-7) for the day of the week: ");
+            string input = Console.ReadLine();
+
+           
+            int dayn=int.Parse(input);
+                DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), dayn.ToString());
+                Console.WriteLine($"The day is: {day}");
+    
+
+
+
+        } }}
